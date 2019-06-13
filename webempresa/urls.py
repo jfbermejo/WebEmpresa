dac38868bd95876293bc988febb5471fc088061b
Juan F. Bermejo <juan.bermejo@cenits.es>
2019-06-13 13:46:29 +0200

    Realizada la app Services

--- webempresa/urls.py
+++ webempresa/urls.py
@@ -15,12 +15,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
 
 urlpatterns = [
     # Paths del admin
     path('admin/', admin.site.urls),
 
+    # Paths de services
+    path('services/', include('services.urls')),
+
     # Paths del core
-    path('', include('core.urls'))
+    path('', include('core.urls')),
 
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
